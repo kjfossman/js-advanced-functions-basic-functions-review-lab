6d00b97e630663f90669a59ae052a766919c6687
Kyle <kjfossman@gmail.com>
2021-05-17 10:17:00 -0800

    commit tree e02bb41a2974a6a53b9f9ff944ae8755ec548fcc

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(boring='go to the office'){
+    return `This Monday, I will ${boring}.`
+}
+
+function wrapAdjective(string="*"){
+    return function(saying='special'){
+        return `You are ${string}${saying}${string}!`
+        
+    }
+}
+let encouragingPromptFunction = wrapAdjective()()
+
+let Calculator = {add: function(a,b){return a+b}, subtract: function(a,b){return a-b},
+multiply: function(a,b){return a*b}, divide: function(a,b){return a/b}}
+
+function actionApplyer(int, array){
+    let a = int
+    for(let i = 0; i < array.length; i++){
+        a = array[i](a)
+    }
+    return a 
+}
